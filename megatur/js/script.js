d4bd5db3afde5cafd6cea7a239de61276aed7d5f
ssssheva <ssssheva9@gmail.com>
2020-09-06 21:50:51 +0300

    first commit

---
+++ megatur/js/script.js
@@ -0,0 +1,32 @@
+//= ../../node_modules/slick-carousel/slick/slick.min.js
+
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+
+/*$('.icon-menu').click(function (event) {
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});*/
+
+$(document).ready(function () {
+	$('.slider').slick({
+		slidesToShow: 3,
+		appendArrows: $('.arrows'),
+	});
+});  
